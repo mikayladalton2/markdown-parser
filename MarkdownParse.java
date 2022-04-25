2320dbd78f4223e3b471023b9fc5c2e53d7d3c74
mikayladalton2 <103148825+mikayladalton2@users.noreply.github.com>
2022-04-24 21:51:05 -0700

    attempt to fix issue(fail)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,13 +11,16 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.lastIndexOf(")")) {
+        while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            if(openBracket == -1){
+                break;
+            }
         }
 
         return toReturn;
