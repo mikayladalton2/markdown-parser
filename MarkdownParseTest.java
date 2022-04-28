f5799d3bb64fb206128ecd76cde94e9d9209254f
mikayladalton2 <103148825+mikayladalton2@users.noreply.github.com>
2022-04-28 13:13:33 -0700

    Add test file

---
+++ MarkdownParseTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import org.junit.*;
+public class MarkdownParseTest {
+
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testMarkdownParse() throws IOException {
+        Path fileName = Path.of("test-file8.md");
+        String content = Files.readString(fileName);
+        assertEquals("Should be the same.",List.of("a link on the first line"),MarkdownParse.getLinks(content));
+
+    }
+
+
+}
